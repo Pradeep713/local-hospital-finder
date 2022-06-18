2a996fd8a4ac92144fc1e92cdef891ca9e4e93ed
Pradeep Mokara <pradeepmokara1806@gmail.com>
2022-06-18 22:35:46 +0530

    Implemented login

--- base/forms.py
+++ base/forms.py
@@ -1,5 +1,8 @@
-from django.forms import CheckboxSelectMultiple, ModelForm, Select
+from django import forms
+from django.forms import CheckboxSelectMultiple, ModelForm
 from .models import Hospital, Mandal, City
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class HospitalForm(ModelForm):
     class Meta:
@@ -37,3 +40,22 @@ class HospitalForm(ModelForm):
                 self.fields['cityName'].queryset = self.instance.mandalName.cityName_set.order_by('cityName')
             except:
                 pass
+
+class UserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    # Should add choice field to UserCreationField
+    CHOICES = [
+        ("User", "User"),
+        ("Hospital", "Hospital")
+    ]
+    choice = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES, required=True)
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    def save(self, commit:True):
+        user =  super(UserCreationForm, self).save(commit = False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
