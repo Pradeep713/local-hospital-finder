9b3d77347ccd7599ff83146c5f1dbd08428f397a
Pradeep Mokara <pradeepmokara1806@gmail.com>
2022-06-19 23:13:47 +0530

    Rearrangement

--- local-hospital-finder-master/base/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import District, Hospital, Mandal, City, Treatement
-
-admin.site.register(District)
-admin.site.register(Mandal)
-admin.site.register(City)
-admin.site.register(Hospital)
-admin.site.register(Treatement)
--- local-hospital-finder-master/readme.txt
+++
@@ -1,3 +0,0 @@
-Hello, This project is web-application
-And Designed by using Django for Backend HTML, CSS for Frontend
-And The Main intention of this project is to providing the Nearest Hospital Information
